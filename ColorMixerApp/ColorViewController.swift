4bb488bdb871d518cd805dd6734e4d2106914ca1
Kamal <kamalios@mail.ru>
2023-02-25 13:06:51 +0300

    Did some extra functions

--- ColorMixerApp/ColorViewController.swift
+++ ColorMixerApp/ColorViewController.swift
@@ -13,8 +13,6 @@ protocol SettingsViewControllerDelegate: AnyObject {
 
 final class ColorViewController: UIViewController {
 
-    
-    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         guard let settingsVC = segue.destination as? SettingsViewController else { return }
         settingsVC.colorVCBc = view.backgroundColor
@@ -23,12 +21,8 @@ final class ColorViewController: UIViewController {
     }
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .brown
-        
-
 
     }
-    
 }
 
 // MARK: - SettingsViewControllerDelegate
--- ColorMixerApp/SettingsViewController.swift
+++ ColorMixerApp/SettingsViewController.swift
@@ -7,9 +7,11 @@
 
 import UIKit
 
+
+
 final class SettingsViewController: UIViewController {
     
-    // MARK: IBOutlet
+    // MARK: - IBOutlet
     
     @IBOutlet var colorMixView: UIView!
     
@@ -21,12 +23,23 @@ final class SettingsViewController: UIViewController {
     @IBOutlet var GreenColorSlider: UISlider!
     @IBOutlet var blueColorSlider: UISlider!
     
+    @IBOutlet var redColorTF: UITextField!
+    @IBOutlet var greenColorTF: UITextField!
+    @IBOutlet var blueColorTF: UITextField!
+    
+    
     var colorVCBc: UIColor!
     
     unowned var delegate: SettingsViewControllerDelegate!
     
+    // MARK: - override func
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        addDoneButtonOnNumpad(textField: redColorTF)
+        addDoneButtonOnNumpad(textField: greenColorTF)
+        addDoneButtonOnNumpad(textField: blueColorTF)
         
         colorMixView.layer.cornerRadius = 20
         
@@ -37,10 +50,14 @@ final class SettingsViewController: UIViewController {
         colorMixView.backgroundColor = colorVCBc
         setSlidersPosition(of: getComponentsOf(colorMixView.backgroundColor ?? .white))
         setValue(for: redColorLabel, greenColorLabel, blueColorLabel)
-        
     }
     
-    // MARK: IBAction
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        view.endEditing(true)
+    }
+    
+    // MARK: - IBAction
     
     @IBAction func DoneButtonPressed() {
         delegate.setNewColor(for: colorMixView.backgroundColor ?? .red)
@@ -51,12 +68,17 @@ final class SettingsViewController: UIViewController {
         switch sender {
         case redColorSlider:
             redColorLabel.text = string(from: sender)
+            redColorTF.text = string(from: sender)
         case GreenColorSlider:
             greenColorLabel.text = string(from: sender)
+            greenColorTF.text = string(from: sender)
         default:
             blueColorLabel.text = string(from: sender)
+            blueColorTF.text = string(from: sender)
         }
     }
+    
+    // MARK: - private funcs
     
     private func setColor() {
         colorMixView.backgroundColor = UIColor(
@@ -75,10 +97,13 @@ final class SettingsViewController: UIViewController {
             switch label {
             case redColorLabel:
                 redColorLabel.text = string(from: redColorSlider)
+                redColorTF.text = redColorLabel.text
             case greenColorLabel:
                 greenColorLabel.text = string(from: GreenColorSlider)
+                greenColorTF.text = greenColorLabel.text
             default:
                 blueColorLabel.text = string(from: blueColorSlider)
+                blueColorTF.text = blueColorLabel.text
             }
         }
     }
@@ -101,3 +126,20 @@ final class SettingsViewController: UIViewController {
     }
 
 }
+// MARK: - SettingsViewController
+
+extension SettingsViewController {
+    
+    private func addDoneButtonOnNumpad(textField: UITextField) {
+        let keypadToolbar = UIToolbar()
+        
+        keypadToolbar.items = [
+            UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.flexibleSpace, target: self, action: nil),
+            UIBarButtonItem(title: "Done", style: UIBarButtonItem.Style.done, target: textField, action: #selector(UITextField.resignFirstResponder))
+        ]
+        keypadToolbar.sizeToFit()
+        textField.inputAccessoryView = keypadToolbar
+    }
+}
+
+
